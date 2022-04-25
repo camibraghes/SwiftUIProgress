d6f5884bb6f4a5755321bfff28a3352c271d38fb
Cami Braghes <camibraghes@gmail.com>
2022-04-25 17:28:15 +0300

    Core Data relationships, predicates, and delete rules, Part l

--- SwiftUIProgress/CoreDataRelation.swift
+++ SwiftUIProgress/CoreDataRelation.swift
@@ -14,7 +14,7 @@ class CoreDataManager {
     let context: NSManagedObjectContext
     
     init() {
-        container = NSPersistentContainer.init(name: "")
+        container = NSPersistentContainer.init(name: "CoreDataContainer")
         container.loadPersistentStores { (description, error) in
             if let error = error {
                 print("Error loading Core Data.\(error)")
@@ -26,14 +26,63 @@ class CoreDataManager {
     func save() {
         do {
             try context.save()
+                print("Saved Successfully")
         } catch let error {
-            print("Error saving Core Data.\(error.localizedDescription)")
+                print("Error saving Core Data.\(error.localizedDescription)")
         }
     }
 }
 
 class CoreDataRelationViewModel: ObservableObject {
     let manager = CoreDataManager.instance
+    @Published var businesses: [BusinessEntity] = []
+    
+    func getBusinesses() {
+        let request = NSFetchRequest<BusinessEntity>(entityName: "BusinessEntity")
+        
+        do {
+            businesses = try manager.context.fetch(request)
+        } catch let error {
+            print("Error fetching.\(error.localizedDescription)")
+        }
+    }
+    
+    func addBusiness() {
+        let newBusiness = BusinessEntity(context: manager.context)
+        newBusiness.name = "Apple"
+        save()
+        
+        //add existing depatments to the new business:
+        //newBusiness.department = []
+        
+        //add existing employee to the new business:
+        //newBusiness.employee = []
+
+        //add new business to existing depatments:
+        //newBusiness.addToDepartments(<#T##value: DepartmentEntity##DepartmentEntity#>)
+        
+        //add new business to existing employee:
+        //newBusiness.addToEmployess(<#T##value: EmployeeEntity##EmployeeEntity#>)
+
+
+    }
+    
+    func addDepartment() {
+        let newDepartment = DepartmentEntity(context: manager.context)
+        newDepartment.name = "Marketing"
+        newDepartment.businesses = [businesses[0]]
+        save()
+    }
+    
+    func save() {
+        businesses.removeAll()
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) {
+            self.manager.save()
+            self.getBusinesses()
+        }
+     
+    }
 }
 
 struct CoreDataRelation: View {
@@ -41,7 +90,31 @@ struct CoreDataRelation: View {
     @StateObject var viewModel = CoreDataRelationViewModel()
     
     var body: some View {
-        Text("")
+        NavigationView {
+            ScrollView {
+                VStack(spacing: 10) {
+                    Button {
+                        viewModel.addDepartment()
+                    } label: {
+                        Text("Perform Action")
+                            .font(.headline)
+                            .foregroundColor(.white)
+                            .frame(height: 55)
+                            .frame(maxWidth: .infinity)
+                            .background(Color.blue.cornerRadius(10))
+                    }
+                    
+                    ScrollView(.horizontal, showsIndicators: true) {
+                        HStack(alignment: .top) {
+                            ForEach(viewModel.businesses) { buisness in
+                                BusinessView(entity: buisness)
+                            }
+                        }
+                    }
+                }
+            }
+            .navigationTitle("Relationships")
+        }
     }
 }
 
@@ -50,3 +123,37 @@ struct CoreDataRelation_Previews: PreviewProvider {
         CoreDataRelation()
     }
 }
+
+struct BusinessView: View {
+        
+    let entity: BusinessEntity
+    
+    var body: some View {
+        VStack(alignment: .leading, spacing: 20) {
+            Text("Name: \(entity.name ?? "" )")
+                .bold()
+            
+            if let departments = entity.departments?.allObjects as? [DepartmentEntity] {
+                Text("Departments:")
+                    .bold()
+                ForEach(departments) { department in
+                    Text(department.name ?? "")
+                }
+            }
+            
+            if let employees = entity.employess?.allObjects as? [EmployeeEntity] {
+                Text("Employees:")
+                    .bold()
+                ForEach(employees) { employee in
+                    Text(employee.name ?? "" )
+                }
+            }
+        }
+        .padding()
+        .frame(maxWidth: 300, alignment: .leading)
+        .background(Color.gray.opacity(0.5))
+        .cornerRadius(10)
+        .shadow(radius: 10)
+    }
+}
+
