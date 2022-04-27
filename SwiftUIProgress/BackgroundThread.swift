04ea0ea580bd5b929bb0dd1524434d87f45c971b
Cami Braghes <camibraghes@gmail.com>
2022-04-27 18:00:06 +0300

    Multi-threading with background threads and queues

---
+++ SwiftUIProgress/BackgroundThread.swift
@@ -0,0 +1,69 @@
+//
+//  BackgroundThread.swift
+//  SwiftUIProgress
+//
+//  Created by Camelia Braghes on 27.04.2022.
+//
+
+import SwiftUI
+
+class BackgroundThreadViewModel: ObservableObject {
+    
+    @Published var dataArray: [String] = []
+    
+    func fetchData() {
+        DispatchQueue.global(qos: .background).async {
+            let newData = self.downloadData()
+
+            print("Check 1: \(Thread.isMainThread)")
+            print("Check 1: \(Thread.current)")
+
+        DispatchQueue.main.async {
+            self.dataArray = newData
+            print("Check 2: \(Thread.isMainThread)")
+            print("Check 2: \(Thread.current)")
+
+
+            }
+        }
+    }
+    
+    private func downloadData() -> [String] {
+        var data: [String] = []
+        for x in 0..<100 {
+            data.append("\(x)")
+            print(data)
+        }
+        return data
+    }
+}
+
+struct BackgroundThread: View {
+    
+    @StateObject var viewModel = BackgroundThreadViewModel()
+    
+    var body: some View {
+        ScrollView {
+            LazyVStack(spacing: 10) {
+                Text("Load Data")
+                    .font(.title2)
+                    .fontWeight(.bold)
+                    .onTapGesture {
+                        viewModel.fetchData()
+                    }
+                
+                ForEach(viewModel.dataArray, id: \.self) { item in
+                    Text(item)
+                        .font(.headline)
+                        .foregroundColor(.red)
+                }
+            }
+        }
+    }
+}
+
+struct BackgroundThread_Previews: PreviewProvider {
+    static var previews: some View {
+        BackgroundThread()
+    }
+}
