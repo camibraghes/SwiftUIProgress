827c9436ac04980e321631b4c5bb6a989820a448
Cami Braghes <camibraghes@gmail.com>
2022-04-28 19:43:47 +0300

    How to use Typealias in Swift

---
+++ SwiftUIProgress/Typealias.swift
@@ -0,0 +1,37 @@
+//
+//  Typealias.swift
+//  SwiftUIProgress
+//
+//  Created by Camelia Braghes on 28.04.2022.
+//
+
+import SwiftUI
+
+struct MovieModel {
+    let title: String
+    let director: String
+    let count: Int
+}
+
+typealias TVModel = MovieModel
+
+struct Typealias: View {
+    
+//    @State var item: MovieModel = MovieModel(title: "Title", director: "Joe", count: 5)
+    @State var item: TVModel = TVModel(title: "TV Show", director: "Emmily", count: 10)
+    
+    var body: some View {
+        VStack {
+            Text(item.title)
+            Text(item.director)
+            Text("\(item.count)")
+        }
+        
+    }
+}
+
+struct Typealias_Previews: PreviewProvider {
+    static var previews: some View {
+        Typealias()
+    }
+}
